0ff875303e85761d0a16f9f06b9bfbc84be49140
naledi mokhethi <naledi.mokhethi@outlook.com>
2025-01-18 16:17:54 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HospitalQueue.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
