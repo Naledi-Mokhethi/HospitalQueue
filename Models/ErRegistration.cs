5cb022c731e405f70a066ced78d4c075600f6ea1
Naledi-Mokhethi <naledimokhethi28@gmail.com>
2025-02-09 08:57:08 +0200

    Updating fields to have an empty string

--- Models/ErRegistration.cs
+++ Models/ErRegistration.cs
@@ -6,17 +6,17 @@ namespace HospitalQueue.Models
     public class ErRegistration
     {
         [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "Only alphabetic characters are allowed.")]
-        public string? FullNames { get; set; }
+        public string FullNames { get; set; } = string.Empty;
         [RegularExpression(@"^\d{1,13}$", ErrorMessage = "Identity Number must be between 1 and 13 digits.")]
-        public string? IdentityNum { get; set; }
+        public string IdentityNum { get; set; } = string.Empty;
         [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "Only alphabetic characters are allowed.")]
-        public  string? LastName { get; set; }
+        public string LastName { get; set; } = string.Empty;
         [RegularExpression(@"^\d{1,10}$", ErrorMessage = "Phone Number must be between 1 and 10 digits.")]
-        public  string? PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; } = string.Empty;
         public DateTime DateTime { get; set; }
 
         public List<KeyValuePair<int, string>>? ProblemPriority { get; set; }
         public int SelectedPriority { get; set; }
-        public string? Description { get; set; }
+        public string Description { get; set; } = string.Empty;
     }
 }
