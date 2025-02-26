5d3ea26f338662c40378d349e17da36ece22eb80
Naledi-Mokhethi <naledimokhethi28@gmail.com>
2025-02-26 21:05:02 +0200

    API Intergration - WIP

---
+++ Models/UserDto/User.cs
@@ -0,0 +1,14 @@
+﻿namespace HospitalQueue.Models.UserDto
+{
+    public class User
+    {
+        public string EmployeeName { get; set; } = string.Empty;
+        public string EmployeeLastName { get; set; } = string.Empty;
+        public string EmployeeDepartment { get; set; } = string.Empty;
+        public string EmloyeeeJobTitle { get; set; } = string.Empty;
+        public string EmployeePhoneNumber { get; set; } = string.Empty;
+        public string EmployeeEmail { get; set; } = string.Empty;
+        public string EmployeePassword { get; set; } = string.Empty;
+
+    }
+}
